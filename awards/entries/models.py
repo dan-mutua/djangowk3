940dded70a99e4b2e8ace26210e72272ce5e2c18
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-10-25 11:43:36 +0300

    add first model for db

--- awards/entries/models.py
+++ awards/entries/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User  
 
 # Create your models here.
+
+class Entry(models.Model):
+  entry_tittle=models.CharField(max_length=60)
+  entry_text=models.TextField()
+  entry_date=models.DateTimeField(auto_now_add=True)
+  entry_author=models.ForeignKey(User, on_delete=models.CASCADE)
