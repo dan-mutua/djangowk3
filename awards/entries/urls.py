f588caf6af0c3fa578665873f49766a186709e45
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-10-25 20:50:39 +0300

    add new class

--- awards/entries/urls.py
+++ awards/entries/urls.py
@@ -1,7 +1,8 @@
-from .views import HomeView
+from .views import HomeView,EntryView
 from django.urls import path
 
 urlpatterns = [
     
-    path('', HomeView.as_view(), name='homed' )
+    path('', HomeView.as_view(), name='homed' ),
+    path('entry/<int:pk>/', EntryView.as_view(), name='entry', ),
 ]
