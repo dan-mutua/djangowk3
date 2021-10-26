61ab3ef53f15d82a9cbfc0370862468ad2fe9573
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-10-26 15:36:29 +0300

    add create page

--- awards/entries/urls.py
+++ awards/entries/urls.py
@@ -1,9 +1,9 @@
-from .views import HomeView,EntryView
+from .views import HomeView,EntryView,CreateView
 from django.urls import path
 
 urlpatterns = [
     
     path('', HomeView.as_view(), name='homed' ),
     path('entry/<int:pk>/', EntryView.as_view(), name='entrydetail', ),
-    path('create/')
+    path('create/',CreateView.as_view(), name='create'),
 ]
