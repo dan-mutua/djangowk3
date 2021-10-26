3983e0ee7e63a579d2fb636aab83a5ac9ad0c7d6
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-10-26 14:52:01 +0300

    add post detail

--- awards/entries/urls.py
+++ awards/entries/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns = [
     
     path('', HomeView.as_view(), name='homed' ),
-    path('entry/<int:pk>/', EntryView.as_view(), name='entry', ),
+    path('entry/<int:pk>/', EntryView.as_view(), name='entrydetail', ),
 ]
